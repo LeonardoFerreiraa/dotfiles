efca0e03e514fcc85104f3557de99b2111b3cca5
leoferreira <mail@leonardoferreira.com.br>
2026-07-07 10:31:15 -0300

    statusline

---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,20 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  event = 'VeryLazy',
+  config = function()
+    require('lualine').setup({
+      options = {
+        theme = 'catppuccin-macchiato',
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diagnostics' },
+        lualine_c = { { 'filename', path = 1 } },
+        lualine_x = { 'filetype' },
+        lualine_y = {},
+        lualine_z = { 'location' },
+      },
+    })
+  end,
+}
